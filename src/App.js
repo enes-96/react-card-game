b2b68929879300ef0de4938b018f8fc34bf64479
Seyit Enes <seyit7enes@gmail.com>
2023-04-15 22:32:54 +0200

    done

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import { useState, useEffect } from "react";
 const API_Key = "https://pokeapi.co/api/v2/";
 
 async function fetchRandomPokemon() {
-  const randomId = Math.floor(Math.random() * 898) + 1; // Get a random Pokemon ID between 1 and 898 (the total number of Pokemon in the PokeAPI)
+  const randomId = Math.floor(Math.random() * 898) + 1;
   const response = await fetch(`${API_Key}pokemon/${randomId}`);
   const data = await response.json();
-  return data;
+  return { ...data, id: randomId };
 }
 
 function shuffleArray(array) {
@@ -22,6 +22,7 @@ function shuffleArray(array) {
 function App() {
   const [pokemonData, setPokemonData] = useState([]);
   const [clickedCards, setClickedCards] = useState([]);
+  const [lives, setLives] = useState(3); // Initialize the number of lives to 3
 
   useEffect(() => {
     async function fetchData() {
@@ -30,18 +31,31 @@ function App() {
         const pokemon = await fetchRandomPokemon();
         data.push(pokemon);
       }
-      setPokemonData(shuffleArray(data)); // shuffle the initial order of the cards
+      setPokemonData(shuffleArray(data));
     }
     fetchData();
   }, []);
 
-  function handleCardClick(index) {
-    if (clickedCards.includes(index)) {
-      alert("You already clicked this card!");
+  function handleCardClick(id) {
+    if (clickedCards.includes(id)) {
+      setLives(lives - 1); // Reduce the number of lives by 1 if the card has already been clicked
+      if (lives === 1) {
+        alert("Game over! You lost all your lives.");
+        setLives(3); // Reset the number of lives to 3 if the game is over
+        setClickedCards([]); // Reset the clicked cards array
+        const shuffledData = shuffleArray([...pokemonData]);
+        setPokemonData(shuffledData);
+        return;
+      } else {
+        alert(
+          `You already clicked this card! You have ${lives - 1} lives left.`
+        );
+      }
     } else {
-      const newClickedCards = [...clickedCards, index];
+      const newClickedCards = [...clickedCards, id];
       setClickedCards(newClickedCards);
-      const shuffledData = shuffleArray([...pokemonData]); // create a new array with the same elements as pokemonData and shuffle it
+      console.log(clickedCards);
+      const shuffledData = shuffleArray([...pokemonData]);
       setPokemonData(shuffledData);
     }
   }
@@ -51,12 +65,15 @@ function App() {
       <div className="header">
         <h1>Memory Card Pokemon</h1>
       </div>
+      <div className="lives">
+        <h2>Lives: {lives}</h2>
+      </div>
       <div className="pokemon-grid">
-        {pokemonData.map((pokemon, index) => (
+        {pokemonData.map((pokemon) => (
           <div
             key={pokemon.id}
             className="pokemon-card"
-            onClick={() => handleCardClick(index)}
+            onClick={() => handleCardClick(pokemon.id)}
           >
             <img src={pokemon.sprites.front_default} alt={pokemon.name} />
             <h2>{pokemon.name}</h2>
